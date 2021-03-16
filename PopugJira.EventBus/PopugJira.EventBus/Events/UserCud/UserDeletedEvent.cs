e6f9c3bdc34dec27c2c4240aa060dec8649f6073
picolino <webmaster.pico@gmail.com>
2021-03-16 19:28:47 +0300

    Create initial eventbus frame

---
+++ PopugJira.EventBus/PopugJira.EventBus/Events/UserCud/UserDeletedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace PopugJira.EventBus.Events.UserCud
+{
+    public class UserDeletedEvent
+    {
+    }
+}
---
+++ PopugJira.EventBus/PopugJira.EventBus/IMessageBus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Serviced;
+
+namespace PopugJira.EventBus
+{
+    public interface IMessageBus : IScoped
+    {
+        Task Publish<TMessage>(TMessage message);
+        Task Subscribe<TMessage>(string subscriptionId, Func<TMessage, Task> onMessage);
+    }
+}
