186523f28ac1ac406fef72d9c5c3c215a3cb64ba
picolino <webmaster.pico@gmail.com>
2021-03-17 19:00:06 +0300

    Produce goal completed event

--- PopugJira.GoalTracker/PopugJira.GoalTracker.Application/Commands/CompleteGoalCommand.cs
+++ PopugJira.GoalTracker/PopugJira.GoalTracker.Application/Commands/CompleteGoalCommand.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Threading.Tasks;
 using PopugJira.AutoDI;
+using PopugJira.Common;
+using PopugJira.EventBus;
+using PopugJira.EventBus.Events.BusinessEvents;
 using PopugJira.GoalTracker.DataAccessLayer.Contract;
 using PopugJira.GoalTracker.Domain.Definitions;
 
@@ -9,15 +12,37 @@ namespace PopugJira.GoalTracker.Application.Commands
     public class CompleteGoalCommand : ICommand
     {
         private readonly IGoalsWriteDbOperations goalsWriteDbOperations;
+        private readonly IGoalsGetDbOperations goalsGetDbOperations;
+        private readonly IGoalsConfigGetDbOperations goalsConfigGetDbOperations;
+        private readonly IDateTimeService dateTimeService;
+        private readonly IMessageBus messageBus;
 
-        public CompleteGoalCommand(IGoalsWriteDbOperations goalsWriteDbOperations)
+        public CompleteGoalCommand(IGoalsWriteDbOperations goalsWriteDbOperations,
+                                   IGoalsGetDbOperations goalsGetDbOperations,
+                                   IGoalsConfigGetDbOperations goalsConfigGetDbOperations,
+                                   IDateTimeService dateTimeService,
+                                   IMessageBus messageBus)
         {
             this.goalsWriteDbOperations = goalsWriteDbOperations;
+            this.goalsGetDbOperations = goalsGetDbOperations;
+            this.goalsConfigGetDbOperations = goalsConfigGetDbOperations;
+            this.dateTimeService = dateTimeService;
+            this.messageBus = messageBus;
         }
         
         public async Task Execute(string id)
         {
             await goalsWriteDbOperations.SetState(GoalState.Complete, id);
+            var completeUtcDateTime = dateTimeService.UtcNow;
+            var goal = await goalsGetDbOperations.Get(id);
+            var goalCompletePrice = await goalsConfigGetDbOperations.GetCompleteGoalPrice();
+            await messageBus.Publish(new GoalCompletedEvent
+                                     {
+                                         Id = id,
+                                         AssigneeId = goal.Assignee.Id,
+                                         CompletePrice = goalCompletePrice,
+                                         CompleteDateTime = completeUtcDateTime
+                                     });
         }
     }
 }
